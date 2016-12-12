ecf269d6b697391b226143a63c6c0aef19ca31af
Antony Siegert <antony.siegert@infopark.de>
2016-12-12 16:20:57 +0100

    adding scrivito

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, :at => '/'
           # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Default Scrivito routes. Adapt them to change the routing of CMS objects.
+  # See the documentation of 'scrivito_route' for a detailed description.
+  scrivito_route '/', using: 'homepage'
+  scrivito_route '(/)(*slug-):id', using: 'slug_id'
+  scrivito_route '/*permalink', using: 'permalink', format: false
 end
